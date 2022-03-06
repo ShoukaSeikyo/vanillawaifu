5ea00776d36e10437ce1e9531fb23a40f3f58d46
Olivier Martinez <shouka0seikyo@gmail.com>
2022-03-06 11:31:12 -0500

    Fixed infinity bucket behavior with cauldrons and dispensers.

--- src/main/java/net/orandja/vw/mixin/BucketItemMixin.java
+++ src/main/java/net/orandja/vw/mixin/BucketItemMixin.java
@@ -15,12 +15,12 @@ public abstract class BucketItemMixin extends Item implements FluidModificationI
 
     @Redirect(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemUsage;exchangeStack(Lnet/minecraft/item/ItemStack;Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/item/ItemStack;)Lnet/minecraft/item/ItemStack;"))
     public ItemStack onEmptyBucket(ItemStack inputStack, PlayerEntity player, ItemStack outputStack) {
-        return handleEmptyInfinityBucket(inputStack, player, outputStack);
+        return Companion.handleEmptyInfinityBucket(inputStack, player, outputStack);
     }
 
     @Redirect(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/BucketItem;getEmptiedStack(Lnet/minecraft/item/ItemStack;Lnet/minecraft/entity/player/PlayerEntity;)Lnet/minecraft/item/ItemStack;"))
     public ItemStack onWaterBucket(ItemStack stack, PlayerEntity player) {
-        return handleWaterInfinityBucket(stack, player);
+        return Companion.handleWaterInfinityBucket(stack, player);
     }
 
 }
